4e9ff02114a5a2f33be923c134a722afb57d924d
JeonghyunLee <sump99@naver.com>
2025-04-24 16:06:33 +0900

    API Request/Response Test
    https://docs.pydantic.dev/latest/api/errors/

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +6,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, Wo23rld!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
